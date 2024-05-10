4c66b94fd2d92cf1dcfa208d348641cc78b143f5
Jon Pitans <jonpitans@gmail.com>
2024-05-10 10:02:48 +0100

    video of dev push

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <h1>CICD Demo</h1>
       <p>This is the dev branch</p>
       <p>new stuff to push to dev</p>
-      <p>another line to push</p>
+      <p>video on dev branch check</p>
     </>
   );
 }
